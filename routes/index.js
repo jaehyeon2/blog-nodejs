5de3bd9bdc8b326dc62b2675917195424a67d954
강재현 <wol5971@gmail.com>
2021-08-01 10:43:22 +0000

    add files

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+const express=require('express')
+
+const {User, Category, Post, Comment}=require('../models');
+const {isLoggedIn, isNotLoggedIn}=require('./middlewares');
+
+const router=express.Router();
+
+router.use((req, res, next)=>{
+	res.locals.user=req.user;
+	next();
+});
+
+router.get('/', async(req, res, next)=>{
+	try{
+		const categories=await Category.findAll({order:['id', 'DESC']});
+		res.render('main',{
+			title:'blog-main',
+			categories,
+		});
+	}catch(error){
+		console.error(error);
+		next(error);
+	}
+});
+
+router.get('/join', isNotLoggedIn, (req, res)=>{
+	res.render('join',{
+		title:'회원가입-blog',
+	});
+});
+
+router.get('/write_post', isLoggedIn, (req, res)=>{
+	res.render('write',{
+		title:'글쓰기-blog',
+	});
+});
